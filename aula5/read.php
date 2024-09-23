d14b3cad87f7ecd33ab46d4e2e2637063cb16735
Mira <mateus_c_mira@estudante.sesisenai.org.br>
2024-09-23 09:17:31 -0300

    eu enviei o db, create e parte do read

---
+++ aula5/read.php
@@ -0,0 +1,39 @@
+<?php
+
+include 'db.php';
+
+$sql = "SELECT * FROM Aulas";
+
+$result = $conn -> query($sql);
+
+if ($result -> num_rows > 0){
+    echo "<table border='1'>
+        <tr>
+            <th> ID </th>
+            <th> Sala </th>
+            <th> Disciplina </th>
+            <th> Data da aula </th>
+            <th> Horário</th>
+            <th> Ações</th>
+        </tr>";
+        while($row = $result -> fetch_assoc()){
+            echo "<tr>
+                    <td> {$row['id_aulas']} </td>
+                    <td> {$row['sala']} </td>
+                    <td> {$row['disciplina']} </td>
+                    <td> {$row['data_aula']} </td>
+                    <td> {$row['horario']} </td>
+                    <td>
+                        <a href='update.php?id={$row['id_aulas']}'>Editar</a> |
+                        <a href='delete.php?id={$row['id_aulas']}'>Excluir</a>
+                    </td>
+                </tr>";
+        }
+    echo "</table>";
+}else{
+    echo "Nenhum registro encontrado.";
+}
+$conn -> close();
+
+?>
+<a href="create.php">Inserir novo registro.</a>
